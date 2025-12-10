ca2c6054d70c4258adaaafff022687830815ecb1
Thigo-Campa <thiagocampagnaro9@gmail.com>
2025-12-09 23:31:34 -0300

    rama pico - seccion discapacidad

---
+++ JavaScript/discapacidad.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tieneDis = document.getElementById("tieneDis");
+  const extra = document.getElementById("discapacidadExtra");
+
+  // Ocultar al inicio
+  extra.style.display = "none";
+
+  tieneDis.addEventListener("change", () => {
+    if (tieneDis.value === "Si") {
+      extra.style.display = "block";
+    } else {
+      extra.style.display = "none";
+    }
+  });
+});
